70ba84c75a0a6016e50e2e6845ea55a502ae70a3
ennalezah <hae.villareal@gmail.com>
2019-07-30 19:18:48 -0700

    working on method to scrape zodiac sign page

--- lib/daily_horoscope/scraper.rb
+++ lib/daily_horoscope/scraper.rb
@@ -1,12 +1,8 @@
 class Sraper 
 
-    def get_index_page
+    def scrape_index_page
         doc = Nokogiri::HTML(open("https://www.horoscope.com/us/index.aspx"))
         zodiac_signs = doc.css("section.choose-zodiac div.grid.grid-6")
-
-        # sign name: zodiac_signs.css("h3").text.strip
-        # sign dob: zodiac_signs.css("p").text.strip
-        # sign profile link: zodiac_signs.css("a").first["href"]
 
         zodiac_signs.map do |sign|
             {
@@ -15,8 +11,9 @@ class Sraper
                 :sign_profile => sign.css("a").first["href"]
             }
         end
+    end
 
-
-
+    def scrape_sign_page
+        
     end
 end
